0e9918f1e6478d354cdab7a76a0e2508821c1515
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:27:53 +0530

    Add project files.

---
+++ ConsoleApp_parameter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp_parameter
+{
+    class Param
+    {
+        public void display(params object[] list)
+        {
+            foreach(object name in list )
+            {
+                Console.WriteLine(name);
+                
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Param obj = new Param();
+            obj.display(101,"Vaishnavi","Pawar",'U','@');
+        }
+    }
+}
